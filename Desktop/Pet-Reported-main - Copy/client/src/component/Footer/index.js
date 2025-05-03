1d1e004e2e5cf3ce63a05cfd4bada3b5d439ce7c
Your Name <kareemhinde8@gmail.com>
2025-05-04 00:35:25 +0200

    project front end

---
+++ Desktop/Pet-Reported-main - Copy/client/src/component/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer style={{ marginTop: "20px", textAlign: "center", color: "#555" }}>
+        © 2025 PawPrint Locator | Helping reunite pets with their families
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
